085d81c139de3df786c1e7abb238d4d4283b5540
JakkeYang <jakke.yang@gmail.com>
2016-03-31 17:45:33 +0800

    add indication text

--- src/com/miluhe/rowsolitaireapp/actors/PlayerAvatar.java
+++ src/com/miluhe/rowsolitaireapp/actors/PlayerAvatar.java
@@ -10,19 +10,39 @@ import com.miluhe.rowsolitaireapp.SolitaireTextureLoader;
  * Created by jakke on 16-1-12.
  */
 public class PlayerAvatar extends Actor {
+    public enum TTalkerPosition {
+        ETop, EBottom, ELeft, ERight
+    };
+
     private String mName;
     private Texture mTexture;
+    private String mTalkerText;
+    private TTalkerPosition mTalkPos = TTalkerPosition.EBottom;
     public static final int KPlayerTextureX = 128;
     public static final int KPlayerTextureY = 128;
+    private float mTalkerStartX;
+    private float mTalerStartY;
 
     public PlayerAvatar(String name) {
         mName = name;
 
         mTexture = SolitaireTextureLoader.instance().load( name );
+
+        if (mTalkPos == TTalkerPosition.EBottom) {
+
+        }
     }
 
     @Override
     public void draw(SpriteBatch batch, float parentAlpha) {
     	batch.draw(mTexture, getX(), getY());
+    }
+
+    public String getmTalkerText() {
+        return mTalkerText;
+    }
+
+    public void setmTalkerText(String mTalkerText) {
+        this.mTalkerText = mTalkerText;
     }
 }
